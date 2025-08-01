f992fa53680d2f610bac16b40851a225f893aa9e
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-08-01 21:54:08 +0530

    changes

--- main.py
+++ main.py
@@ -1,67 +1,32 @@
-import speech_recognition as sr
-import pyttsx3
 import datetime
-import wikipedia
 import webbrowser
-import os
-import pyjokes
-
-def speak(text):
-    print(f"Assistant: {text}")
-    try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-    except:
-        speak("Speech output not supported in Colab.")
 
 def wish_user():
     hour = int(datetime.datetime.now().hour)
     if hour < 12:
-        speak('Good Morning!')
+        return "Good morning"
     elif hour < 18:
-        speak('Good Afternoon!')
+        return "Good afternoon"
     else:
-        speak('Good Evening!')
-    speak("I am your voice assistant. How can i help you today?")
+        return "Good evening"
 
-def take_command():
-    return input("You (type your command): ").lower()
-
-def run_assistant():
-    wish_user()
+def run_assistant(query):
     while True:
-        query = take_command()
-
-        if 'wikipedia' in query:
-            speak("Searching Wikipedia...")
-            query = query.replace("wikipedia", "")
-            try:
-                result = wikipedia.summary(query, sentences=2)
-                speak("According to Wikipedia:")
-                speak(result)
-            except:
-                speak("Sorry, I couldn't find anything.")
-
-        elif 'open youtube' in query:
-            speak("Opening YouTube...")
+        if 'open youtube' in query:
+            return "Opening YouTube..."
             webbrowser.open("https://www.youtube.com/")
 
         elif 'open google' in query:
-            speak("Opening Google...")
+            return "Opening Google..."
             webbrowser.open("https://www.google.com/")
 
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"The current time is {strTime}")
-
-        elif 'joke' in query:
-            joke = pyjokes.get_joke()
-            speak(joke)
+            return f"The current time is {strTime}"
 
         elif 'exit' in query or 'bye' in query:
-            speak("Goodbye! Have a nice day!")
+            return "Goodbye! Have a nice day!"
             break
 
         else:
-            speak("Sorry, I didn't understand that. Try again.")
+            return "Sorry, I didn't understand that. Try again."
